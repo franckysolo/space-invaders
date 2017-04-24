b87bef2c0ee4451b90493651239de4d4ca64d4ef
franckysolo <franckysolo@gmail.com>
2017-04-25 00:57:25 +0200

    Add The player class

---
+++ src/lib/game/player.js
@@ -0,0 +1,49 @@
+/**
+ * @author franckysolo
+ */
+ import BasePlayer from '../core/Player'
+
+ const defaultOptions = {
+   level: 1,
+   score: 0,
+   shot: 0,
+   shotRemain: 55,
+   life: 3
+ }
+ /**
+  * The space-invaders player
+  * @type {BasePlayer}
+  */
+ class Player extends BasePlayer {
+   /**
+    * Create a new player
+    * @param  {String} name the player's name
+    * @return void
+    */
+   constructor (name) {
+     super(name, defaultOptions)
+     this.name = name || 'Player 1'
+     this.init()
+   }
+   /**
+    * Init the player params
+    * @return void
+    */
+   init () {
+     this.level = this.options.level
+     this.score = this.options.score
+     this.shot = this.options.shot
+     this.life = this.options.life
+     this.shotRemain = this.options.shotRemain
+   }
+   /**
+    * Re-Init the player params when changing level
+    * @return void
+    */
+   changeLevel () {
+     this.shot = this.options.shot
+     this.shotRemain = this.options.shotRemain
+   }
+ }
+
+ export { Player as default }
