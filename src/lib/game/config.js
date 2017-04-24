c4ae4eb8ecec0c525d7ca04911d7a8f0b8f36b0d
franckysolo <franckysolo@gmail.com>
2017-04-25 00:46:59 +0200

    Add Game config

---
+++ src/lib/game/config.js
@@ -0,0 +1,55 @@
+/**
+ * @author franckysolo
+ *
+ * @description The config game file
+ */
+
+// Sizes sprites
+export const SZ_ROW = 4
+export const SZ_COL = 11
+export const SZ_SPRITE_Y = 32
+
+// Game modes
+export const GAME_PLAY = 0
+export const GAME_PAUSE = 1
+export const GAME_OVER = 2
+export const GAME_REPLAY = 3
+
+// ship mode
+export const SHIP_NORMAL = 0
+export const SHIP_EXPLODE = 1
+
+// red ship mode
+export const RED_SHIP_NORMAL = 0
+export const RED_SHIP_EXPLODE = 1
+export const RED_SHIP_EMPTY = 2
+export const RED_BOMB_FIRE = 3
+
+// invaders mode
+export const INV_OPEN = 0
+export const INV_CLOSE = 1
+export const INV_DESTROY = 2
+export const INV_EMPTY = 3
+
+// directions
+export const DIRECTION_LEFT = 0
+export const DIRECTION_RIGHT = 1
+export const DIRECTION_DOWN = 2
+
+// laser
+export const LASER_ARMED = 0
+export const LASER_FIRE = 1
+export const LASER_HIT = 2
+
+// Bomb
+export const BOMB_OPEN = 0
+export const BOMB_CLOSE = 1
+export const BOMB_ARMED = 2
+export const BOMB_FIRE = 3
+export const BOMB_HIT = 4
+
+// SOUNDS
+export const SOUND_SHOOT = 0
+export const SOUND_BOMB = 1
+export const SOUND_EXPLOSION = 2
+export const SOUND_HIT = 3
